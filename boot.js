749cc6b3cdd02d81126f32fbd9b241aefedddad8
RubaElkhaldy <ro2014baa@gmail.com>
2023-01-14 00:53:34 +0300

    first commit

---
+++ boot.js
@@ -0,0 +1,18 @@
+document.write("hello");
+window.document.write("hello")
+console.log(alert("اهلااا وسهلااااااا"));
+console.error("hi");
+window.alert("hi")
+window.print();
+//كلهممobject
+//window فيها كل الاوامر هوا كل المتصفح كاملا
+//document :ع html
+
+//premative string  number boolean null
+
+//letم بقدر اعدل  عليها
+//var
+//constمثل رقم البطاقة 
+
+let name="ruby";
+console.log('${name}');
